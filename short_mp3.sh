af22668f9129f20dec6404a7b4aa0a9b9ceae46d
Rafiko <ralfcontreras@gmail.com>
2026-09-23 20:40:30 -0500

    FadeOut ffmpeg

--- short_mp3.sh
+++ short_mp3.sh
@@ -2,6 +2,13 @@
 
 # ffmpeg -i entrada.mp3 -ss 00:00:10 -to 00:00:50 -c copy salida.mp3
 
+# FADE OUT CON FFMPEG USANDO EL FILTRO DE AUDIO AFADE
+# ffmpeg -i entrada.mp3 -af "afade=t=out:st=115:d=5" salida.mp3
+# entrada.mp3: Especifica tu archivo de audio original.
+# afade=t=out: Indica que el tipo de efecto (t) es un fundido de salida (out).
+# st=115: Es el Start Time (tiempo de inicio). El fade out empezará en el segundo 115 (1 minuto y 55 segundos). Debes calcular este número restando la duración del fade al tiempo total de tu audio.
+# d=5: Es la Duration (duración). El volumen tardará 5 segundos en bajar por completo hasta llegar a cero.
+
 cd /tmp
 rm -rf short_$1
 
